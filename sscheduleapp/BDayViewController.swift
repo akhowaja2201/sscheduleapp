8990a840cf3d10562f35b6d231fa866255659d57
Jinsungerman <jsagisaka5321@stu.d214.org>
2022-05-02 14:22:01 -0500

    Made lunch storyboard

---
+++ sscheduleapp/BDayViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BDayViewController.swift
+//  sscheduleapp
+//
+//  Created by Jinsung Sagisaka on 5/2/22.
+//
+
+import UIKit
+
+class BDayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
--- sscheduleapp/ViewController.swift
+++ sscheduleapp/ViewController.swift
@@ -10,15 +10,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    var list = ["1st Period", "2nd period", "3rd period"]
+    var listAday = ["1st Period", "2nd period", "3rd period", "4th period"]
+    var listBday = ["5th period", "6th period", "7th period","8th period"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-
-    @IBAction func addButton(_ sender: Any) {
     }
 }
 
