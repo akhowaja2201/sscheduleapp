30fe542352aecb06d070089a807e1bc6161932e1
Jinsungerman <jsagisaka5321@stu.d214.org>
2022-05-02 14:56:14 -0500

    Made it so that classes can be added to A-day table view

--- sscheduleapp/ViewController.swift
+++ sscheduleapp/ViewController.swift
@@ -7,15 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var aDayClassTextView: UITextField!
+    @IBOutlet weak var aDayTableView: UITableView!
 
-    @IBOutlet weak var tableView: UITableView!
+    var list = [""]
     var listAday = ["1st Period", "2nd period", "3rd period", "4th period"]
     var listBday = ["5th period", "6th period", "7th period","8th period"]
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        aDayTableView.dataSource = self
+        aDayTableView.separatorColor = UIColor.cyan
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+            cell.textLabel?.text = list[indexPath.row]
+            return cell
+    }
+    @IBAction func whenClassAddButtonPressed(_ sender: Any) {
+        if let newItem = aDayClassTextView.text, aDayClassTextView.text != "" {
+            list.append(newItem)
+            aDayTableView.reloadData()
+            aDayClassTextView.text = ""
+        } else {
+            
+        }
+        
+    }
+    
+    
 }
 
