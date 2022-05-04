8874769223c1f161b9ecf87b05c7606b0f835829
Alisaad Khowaja <akhowaja2201@stu.d214.org>
2022-05-04 14:26:43 -0500

    added code for b day view controller

--- sscheduleapp/BDayViewController.swift
+++ sscheduleapp/BDayViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class BDayViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-    }
+class BDayViewController: UIViewController, UITableViewDataSource {
+ 
+    @IBOutlet weak var bdayTableView: UITableView!
+    @IBOutlet weak var bDayTextField: UITextField!
     
 
+
+   
+    var list = [""]
+    var listAday = ["1st Period", "2nd period", "3rd period", "4th period"]
+    var listBday = ["5th period", "6th period", "7th period","8th period"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bdayTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+            cell.textLabel?.text = list[indexPath.row]
+            return cell
+    }
+
+    @IBAction func whenAddButtonPressed(_ sender: Any) {
+    
+    if let newItem = bDayTextField.text, bDayTextField.text != "" {
+            list.append(newItem)
+            bdayTableView.reloadData()
+            bDayTextField.text = ""
+        } else {
+            
+        }
+        
+    }
 }
+    
+
+    
+    
+    
+    
+   
+
+        
+        
+    
+    
+
+
+    
+    
+    
+
+
+
