1633ef605ebb75a189ff06a06f5af532bf259ea8
Jinsungerman <jsagisaka5321@stu.d214.org>
2022-05-10 14:01:15 -0500

    made custom schedule view controller and class

--- sscheduleapp/calenderViewController.swift
+++ sscheduleapp/calenderViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
-class calenderViewController: UIViewController {
+class calenderViewController: UIViewController, UITableViewDataSource {
     
+    
+    
+    @IBOutlet weak var calendarTableView: UITableView!
     @IBOutlet weak var eventTextField: UITextField!
     @IBOutlet weak var dayTextField: UITextField!
     
@@ -20,6 +23,15 @@ class calenderViewController: UIViewController {
     }
     @IBAction func whenApplyButtonPressed(_ sender: Any) {
         
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return week.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = calendarTableView.dequeueReusableCell(withIdentifier: "calendarCell", for: indexPath)
+        cell.textLabel?.text = week[indexPath.row]
+        return cell
     }
     
 }
