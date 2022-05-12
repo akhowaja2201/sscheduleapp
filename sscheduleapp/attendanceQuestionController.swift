2b53936e3160b29e0a9aff01543f663c44218d0f
Jinsungerman <jsagisaka5321@stu.d214.org>
2022-05-12 14:55:29 -0500

    made attendance question view controller

---
+++ sscheduleapp/attendanceQuestionController.swift
@@ -0,0 +1,17 @@
+//
+//  attendanceQuestionController.swift
+//  sscheduleapp
+//
+//  Created by Jinsung Sagisaka on 5/12/22.
+//
+
+import UIKit
+
+class attendanceQuestionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
