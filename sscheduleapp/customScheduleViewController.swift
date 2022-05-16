1437a6daf40b4922825368247d011f2a5747e251
Jinsungerman <jsagisaka5321@stu.d214.org>
2022-05-16 14:06:16 -0500

    confirmed custom schedule works

--- sscheduleapp/customScheduleViewController.swift
+++ sscheduleapp/customScheduleViewController.swift
@@ -12,25 +12,27 @@ class customScheduleViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var customTableView: UITableView!
     @IBOutlet weak var customTextField: UITextField!
     
-    var data = [" "]
+    var list = [" "]
+    
     override func viewDidLoad(){
         super.viewDidLoad()
+        customTableView.dataSource = self
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return data.count
+        return list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = customTableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath)
-        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.text = list[indexPath.row]
         return cell
     }
     
     @IBAction func whenAddButtonPressed(_ sender: Any) {
         if let newItem = customTextField.text, customTextField.text != "" {
-                data.append(newItem)
+                list.append(newItem)
                 customTableView.reloadData()
                 customTextField.text = ""
             } else {
