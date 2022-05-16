341da555fdaae72ac76870fe1f0162140d3f12b3
Jinsungerman <jsagisaka5321@stu.d214.org>
2022-05-16 14:37:50 -0500

    made ibactions and iboutlets to objects in attendance view controller story board to attendance view controller code

--- sscheduleapp/attendanceQuestionController.swift
+++ sscheduleapp/attendanceQuestionController.swift
@@ -8,22 +8,16 @@
 import UIKit
 
 class attendanceQuestionController: UIViewController {
-
+    @IBOutlet weak var questionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func whenPickButtonPressed(_ sender: Any) {
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
