ee69bc51966241093a76f46461ff10ef8de0bbd5
Jinsungerman <jsagisaka5321@stu.d214.org>
2022-05-16 14:09:35 -0500

    made attendance question controller and class

--- sscheduleapp/attendanceQuestionController.swift
+++ sscheduleapp/attendanceQuestionController.swift
@@ -2,7 +2,7 @@
 //  attendanceQuestionController.swift
 //  sscheduleapp
 //
-//  Created by Jinsung Sagisaka on 5/12/22.
+//  Created by Jinsung Sagisaka on 5/16/22.
 //
 
 import UIKit
@@ -12,6 +12,18 @@ class attendanceQuestionController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
     }
     
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
