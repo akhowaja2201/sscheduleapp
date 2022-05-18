71b308151c1e1ea1ee910a0cc7fa918acd9ea619
Jinsungerman <jsagisaka5321@stu.d214.org>
2022-05-18 14:24:35 -0500

    made attendance question view controller show random attendance questions for the beginning of class :D

--- sscheduleapp/attendanceQuestionController.swift
+++ sscheduleapp/attendanceQuestionController.swift
@@ -10,13 +10,16 @@ import UIKit
 class attendanceQuestionController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     
+    var questionList = ["Which fruit tastes the best?","what's your favorite breakfast food?","what's your favorite vegetable?",]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     @IBAction func whenPickButtonPressed(_ sender: Any) {
-        
+        let randomNumber = arc4random_uniform(3)
+        questionLabel.text = questionList[Int(randomNumber)]
         
     }
     
