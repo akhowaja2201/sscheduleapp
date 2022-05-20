e7559465218c5f1c0a2d5e9b549e2e6f13698abb
Jinsungerman <jsagisaka5321@stu.d214.org>
2022-05-20 13:58:39 -0500

    made aboutCreatorsController

---
+++ sscheduleapp/aboutCreatorsController.swift
@@ -0,0 +1,29 @@
+//
+//  aboutCreatorsController.swift
+//  sscheduleapp
+//
+//  Created by Jinsung Sagisaka on 5/20/22.
+//
+
+import UIKit
+
+class aboutCreatorsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
